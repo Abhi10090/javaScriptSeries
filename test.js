f88d521a5b0c5c0c56480aef0605fd4cf095b6d6
Abhi10090 <aabhi5076@gmail.com>
2024-07-25 08:34:35 +0000

    conversion of  datat type

--- test.js
+++ test.js
@@ -1,1 +1,26 @@
-console.log("Abhishek");
+// console.log("Abhishek");
+//type converson
+//number
+
+// let score = 33;
+// console.log(typeof score);
+// let nuberInString = String(score);
+// console.log(typeof nuberInString);
+
+
+// let score  = "33abc";
+// console.log(typeof score);
+// let stringInNumber = Number(score);
+// console.log(typeof stringInNumber);
+// console.log(stringInNumber);
+
+//if we want to conver a string in a nubmber 
+// "33" => 33 // number ;
+// "33abc" => number // NaN 
+//"abhishek" => number // NaN;
+
+
+//conversion to boolean 
+ let score = "abhi";
+ let logginInBoolean = Boolean(score);
+ console.log( logginInBoolean);
